3bea7684d8064771c663193900fdd89b433e2f40
root <root@instance-20240725-153441.us-west1-b.c.glassy-rush-430401-r4.internal>
2024-10-02 18:08:28 -0700

    Theme updates

--- index.php
+++ index.php
@@ -1,127 +1,152 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-GBW8R6L5BW"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<?php include 'frontend/header.php'; ?>
 
-  gtag('config', 'G-GBW8R6L5BW');
-</script>
+<body>
+
 <?php
+	// Start YOURLS engine
+	require_once( dirname(__FILE__).'/includes/load-yourls.php' );
 
-/*
- * This is an example file for a public interface and a bookmarklet. It
- * is provided so you can build from it and customize to suit your needs.
- * It's not really part of the project. Don't submit feature requests
- * about this file. It's _your_ job to make it what you need it to be :)
- *
- * Rename to .php
- *
- */
+	// URL of the public interface
+	$page = YOURLS_SITE . '/index.php' ;
 
-// Start YOURLS engine
-require_once( dirname(__FILE__).'/includes/load-yourls.php' );
+	// Make variables visible to function & UI
+	$shorturl = $message = $title = $status = '';
 
-// Change this to match the URL of your public interface. Something like: http://your-own-domain-here.com/index.php
-$page = YOURLS_SITE . '/index.php' ;
-
-// Part to be executed if FORM has been submitted
-if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'https://' ) {
-
-	// Get parameters -- they will all be sanitized in yourls_add_new_link()
-	$url     = $_REQUEST['url'];
-	$keyword = isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' ;
-	$title   = isset( $_REQUEST['title'] ) ?  $_REQUEST['title'] : '' ;
-	$text    = isset( $_REQUEST['text'] ) ?  $_REQUEST['text'] : '' ;
-
-	// Create short URL, receive array $return with various information
-	$return  = yourls_add_new_link( $url, $keyword, $title );
-
-	$shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
-	$message  = isset( $return['message'] ) ? $return['message'] : '';
-	$title    = isset( $return['title'] ) ? $return['title'] : '';
-	$status   = isset( $return['status'] ) ? $return['status'] : '';
-
-	// Stop here if bookmarklet with a JSON callback function ("instant" bookmarklets)
-	if( isset( $_GET['jsonp'] ) && $_GET['jsonp'] == 'yourls' ) {
-		$short = $return['shorturl'] ? $return['shorturl'] : '';
-		$message = "Short URL (Ctrl+C to copy)";
-		header('Content-type: application/json');
-		echo yourls_apply_filter( 'bookmarklet_jsonp', "yourls_callback({'short_url':'$short','message':'$message'});" );
-
-		die();
-	}
-}
-
-// Insert <head> markup and all CSS & JS files
-yourls_html_head();
-
-// Display title
-echo "<h1>TRUF.US | Spittin' TRUF Yo!</h1>\n";
-
-// Display left hand menu
-//yourls_html_menu() ;
-
-// Part to be executed if FORM has been submitted
-if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'https://' ) {
-
-	// Display result message of short link creation
-	if( isset( $message ) ) {
-		echo "<h2>$message</h2>";
+	// Part to be executed if FORM has been submitted
+	if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'http://' ) {
+		if (enableRecaptcha) {
+			// Use reCAPTCHA
+			$token = $_POST['token'];
+			$action = $_POST['action'];
+			
+			// call curl to POST request
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => recaptchaV3SecretKey, 'response' => $token)));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$response = curl_exec($ch);
+			curl_close($ch);
+			$arrResponse = json_decode($response, true);
+			
+			// verify the response
+			if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.5) {
+				// reCAPTCHA succeeded
+				shorten();
+			} else {
+				// reCAPTCHA failed
+				$message = "reCAPTCHA failed";
+			}
+		} else {
+			// Don't use reCAPTCHA
+			shorten();
+		}
 	}
 
-	if( $status == 'success' ) {
-		// Include the Copy box and the Quick Share box
-		yourls_share_box( $url, $shorturl, $title, $text );
+	function shorten() {
+		// Get parameters -- they will all be sanitized in yourls_add_new_link()
+		$url     = $_REQUEST['url'];
+		$keyword = isset( $_REQUEST['keyword'] ) ? $_REQUEST['keyword'] : '' ;
+		$title   = isset( $_REQUEST['title'] ) ?  $_REQUEST['title'] : '' ;
+		$text    = isset( $_REQUEST['text'] ) ?  $_REQUEST['text'] : '' ;
 
-		// Initialize clipboard -- requires js/share.js and js/clipboard.min.js to be properly loaded in the <head>
-		echo "<script>init_clipboard();</script>\n";
+		// Create short URL, receive array $return with various information
+		$return  = yourls_add_new_link( $url, $keyword, $title );
+		
+		// Make visible to UI
+		global $shorturl, $message, $status, $title;
+
+		$shorturl = isset( $return['shorturl'] ) ? $return['shorturl'] : '';
+		$message  = isset( $return['message'] ) ? $return['message'] : '';
+		$title    = isset( $return['title'] ) ? $return['title'] : '';
+		$status   = isset( $return['status'] ) ? $return['status'] : '';
+		
+		// Stop here if bookmarklet with a JSON callback function ("instant" bookmarklets)
+		if( isset( $_GET['jsonp'] ) && $_GET['jsonp'] == 'yourls' ) {
+			$short = $return['shorturl'] ? $return['shorturl'] : '';
+			$message = "Short URL (Ctrl+C to copy)";
+			header('Content-type: application/json');
+			echo yourls_apply_filter( 'bookmarklet_jsonp', "yourls_callback({'short_url':'$short','message':'$message'});" );
+			die();
+		}
 	}
-
-// Part to be executed when no form has been submitted
-} else {
-
-		$site = YOURLS_SITE;
-
-		// Display the form
-		echo <<<HTML
-		<h2>Enter a new URL to shorten</h2>
-		<form method="post" action="">
-		<p><label>URL: <input type="text" class="text" name="url" /value="https://" ></label></p>
-		<!--
-		<p><label>Optional custom short URL: $site/<input type="text" class="text" name="keyword" /></label></p>
-		<p><label>Optional title: <input type="text" class="text" name="title" /></label></p>
-		-->
-		<p class="cf-turnstile" data-sitekey="0x4AAAAAAAwhabRTvVD3iN5A" data-callback="javascriptCallback"></p>
-		<p><input type="submit" class="button primary" value="Shorten" /></p>
-		</form>
-HTML;
-
-}
-
 ?>
-<!--
-<h2>Bookmarklets</h2>
 
-<p>Bookmark these links:</p>
+	<div class="container-fluid h-100">
+		<div class="row justify-content-center align-items-center h-100">
+			<div class="col-12 col-lg-10 col-xl-8 col-xxl-5 mt-5">
+				<div class="card border-0 mt-5">
+					<?php if( isset($status) && $status == 'success' ):  ?>
+						<?php $url = preg_replace("(^https?://)", "", $shorturl );  ?>
 
-<p>
+						<div class="close-container text-end mt-3 me-3">
+							<button type="button" class="btn-close" id="close-shortened-screen" aria-label="Close"></button>
+						</div>
 
-<a href="javascript:(function()%7Bvar%20d=document,w=window,enc=encodeURIComponent,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),s2=((s.toString()=='')?s:enc(s)),f='<?php echo $page; ?>',l=d.location,p='?url='+enc(l.href)+'&title='+enc(d.title)+'&text='+s2,u=f+p;try%7Bthrow('ozhismygod');%7Dcatch(z)%7Ba=function()%7Bif(!w.open(u))l.href=u;%7D;if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else%20a();%7Dvoid(0);%7D)()" class="bookmarklet">Default</a>
+						<div class="card-body px-5 pb-5">
+							<h2 class="text-uppercase text-center">Your shortened link</h2>
+							
+							<div class="row justify-content-center">
+								<div class="col-10">
+									<div class="input-group input-group-block mt-4 mb-3">
+										<input type="text" class="form-control text-uppercase" value="<?php echo $shorturl; ?>" required>
+										<button class="btn btn-primary text-uppercase py-2 px-5 mt-2 mt-md-0" type="submit" id="copy-button" data-shorturl="<?php echo $shorturl; ?>">Copy</button>
+									</div>
+									<span class="info">View info &amp; stats at <a href="<?php echo $shorturl; ?>+"><?php echo $url; ?>+</a></span>
+								</div>
+							</div>
+						</div>
+					<?php else: ?>
+						<div class="text-center">
+						<!-- Michael	<img src="<?php echo YOURLS_SITE ?><?php echo logo ?>" alt="Logo" width="95px" class="mt-n5"> -->
+						</div>
+						<div class="card-body px-md-5">
+							<p><?php echo description ?></p>
 
-<a href="javascript:(function()%7Bvar%20d=document,w=window,enc=encodeURIComponent,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),s2=((s.toString()=='')?s:enc(s)),f='<?php echo $page; ?>',l=d.location,k=prompt(%22Custom%20URL%22),k2=(k?'&keyword='+k:%22%22),p='?url='+enc(l.href)+'&title='+enc(d.title)+'&text='+s2+k2,u=f+p;if(k!=null)%7Btry%7Bthrow('ozhismygod');%7Dcatch(z)%7Ba=function()%7Bif(!w.open(u))l.href=u;%7D;if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else%20a();%7Dvoid(0)%7D%7D)()" class="bookmarklet">Custom</a>
+							<?php if ( isset( $_REQUEST['url'] ) && $_REQUEST['url'] != 'http://' ): ?>
+								<?php if (strpos($message,'added') === false): ?>
+									<div class="alert alert-danger alert-dismissible fade show" role="alert">
+										<span>Oh no, <?php echo $message; ?>!</span>
+										<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+									</div>	    
+								<?php endif; ?>
+							<?php endif; ?>
 
-<a href="javascript:(function()%7Bvar%20d=document,s=d.createElement('script');window.yourls_callback=function(r)%7Bif(r.short_url)%7Bprompt(r.message,r.short_url);%7Delse%7Balert('An%20error%20occurred:%20'+r.message);%7D%7D;s.src='<?php echo $page; ?>?url='+encodeURIComponent(d.location.href)+'&jsonp=yourls';void(d.body.appendChild(s));%7D)();" class="bookmarklet">Popup</a>
+							<form id="shortenlink" method="post" action="">
+								<div class="input-group input-group-block mt-4 mb-3">
+									<input type="url" name="url" id="url" class="form-control text-uppercase" placeholder="PASTE URL, SHORTEN &amp; SHARE" aria-label="PASTE URL, SHORTEN &amp; SHARE" aria-describedby="shorten-button" required>
+									<input class="btn btn-primary text-uppercase py-2 px-4 mt-2 mt-md-0" type="submit" id="shorten-button" value="Shorten" />
+								</div>
+								<?php if (enableCustomURL): ?>
+									<a class="btn btn-sm btn-white text-black-50 text-uppercase" data-bs-toggle="collapse" href="#customise-link" role="button" aria-expanded="false" aria-controls="customise-link">
+										<img src="<?php echo YOURLS_SITE ?>/frontend/assets/svg/custom-url.svg" alt="Options"> Customise Link
+									</a>
+									<div class="collapse" id="customise-link">
+										<div class="mt-2 card card-body">
+											<div class="d-flex  align-items-center">
+												<span class="me-2"><?php echo preg_replace("(^https?://)", "", YOURLS_SITE ); ?>/</span>
+												<input type="text" name="keyword" class="form-control form-control-sm text-uppercase" placeholder="CUSTOM URL" aria-label="CUSTOM URL">
+											</div>
+										</div>
+									</div>
+								<?php endif; ?>
+							</form>
+						</div>
+					<?php endif; ?>
+				</div>
+				<div class="d-flex flex-column flex-md-row align-items-center my-3">
+					<span class="text-white fw-light">&copy; <?php echo date("Y"); ?> <?php echo shortTitle ?></span>
+					<div class="ms-3">
+						<?php foreach ($footerLinks as $key => $val): ?>
+							<a class="bold-link me-3 text-white text-decoration-none" href="<?php echo $val ?>"><span><?php echo $key ?></span></a>
+						<?php endforeach ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-<a href="javascript:(function()%7Bvar%20d=document,k=prompt('Custom%20URL'),s=d.createElement('script');if(k!=null){window.yourls_callback=function(r)%7Bif(r.short_url)%7Bprompt(r.message,r.short_url);%7Delse%7Balert('An%20error%20occurred:%20'+r.message);%7D%7D;s.src='<?php echo $page; ?>?url='+encodeURIComponent(d.location.href)+'&keyword='+k+'&jsonp=yourls';void(d.body.appendChild(s));%7D%7D)();" class="bookmarklet">Custom Popup</a>
+	<?php include 'frontend/footer.php'; ?>
 
-</p>
-
-<h2>Please note</h2>
-
-<p>Be aware that a public interface <strong>will</strong> attract spammers. You are strongly advised to install anti spam plugins and any appropriate counter measure to deal with this issue.</p>
--->
-<?php
-
-// Display page footer
-//yourls_html_footer();
+</body>
+</html>
